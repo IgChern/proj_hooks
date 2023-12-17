391d30ae3956c6da1ac6847af7fd5bc6c729d52d
Igor <igorchernysh007@yandex.ru>
2023-12-17 12:32:57 -0300

    test

--- app_hooks/tasks.py
+++ app_hooks/tasks.py
@@ -1,20 +1,16 @@
 from celery import shared_task
-from .models import Event
+from .webhook import Service
 import logging
 
 logger = logging.getLogger(__name__)
 
 
 @shared_task
-def process_jira_callback_task(event_id):
+def process_jira_callback_task(data):
     try:
-        event = Event.objects.get(id=event_id)
-        filters = event.filters.all()
-
-        for filter_obj in filters:
-            logger.info(
-                f"Event: {event.name}, Filter: {filter_obj.name}")
-    except Event.DoesNotExist:
-        logger.error(f"Event ID {event_id} is not found")
+        webhook_service = Service()
+        result = webhook_service.process_jira_callback(data)
+        return result
     except Exception as e:
-        logger.error(f"Error {event_id}: {e}")
+        logger.error(f"An error: {e}")
+        return None
