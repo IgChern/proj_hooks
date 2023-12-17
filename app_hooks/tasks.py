d1b91c3378ebae8dba447c402ea9e24ccab88f42
Igor <igorchernysh007@yandex.ru>
2023-12-17 12:39:42 -0300

    test

--- app_hooks/tasks.py
+++ app_hooks/tasks.py
@@ -6,10 +6,10 @@ logger = logging.getLogger(__name__)
 
 
 @shared_task
-def process_jira_callback_task(data):
+def process_jira_callback_task():
     try:
         webhook_service = Service()
-        result = webhook_service.process_jira_callback(data)
+        result = webhook_service.process_jira_callback()
         return result
     except Exception as e:
         logger.error(f"An error: {e}")
